36a6c09de0ad51e2ebf0061ff9c5930e2e244a1a
noahmiszewski-source <noah.miszewski@skola.taby.se>
2025-11-06 13:02:41 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -7,10 +7,23 @@ print("Hej", namn)
 
 from random import randint
 
-tal = randint(1, 10)
-Gissning = int(input("Gissa ett tal mellan 1 till 10: "))
-if Gissning == tal:
-	print("Rätt gissat!")
-else:
-	print("Fel gissat, talet var", tal)
+#tal = randint(1, 10)
+#Gissning = int(input("Gissa ett tal mellan 1 till 10: "))
+#if Gissning == tal:
+	#print("Rätt gissat!")
+#else:
+#	print("Fel gissat, talet var", tal)
 
+#del 3 - Flera försök
+from random import randint
+tal = randint(1, 10)
+Gissning = int(input("Gissa ett tal mellan 1 till 10. Du har 3 försök: "))
+försök = 1
+while Gissning != tal and försök < 3:
+    print("Fel gissat, försök igen.")
+    Gissning = int(input("Gissa ett tal mellan 1 till 10: "))
+    försök += 1
+if Gissning == tal:
+    print("Rätt gissat!")
+else:
+    print("Fel gissat, talet var", tal)
