1c3e06fe7ee395901550e5fb0f72af7cea02dde4
noahmiszewski-source <noah.miszewski@skola.taby.se>
2025-11-06 12:46:33 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,2 +1,16 @@
+# del 1 - Hälsning
+
 namn = input("vad heter du? ")
 print("Hej", namn)
+
+# del 2 - Slumpa ett tal
+
+from random import randint
+
+tal = randint(1, 10)
+Gissning = int(input("Gissa ett tal mellan 1 till 10: "))
+if Gissning == tal:
+	print("Rätt gissat!")
+else:
+	print("Fel gissat, talet var", tal)
+
