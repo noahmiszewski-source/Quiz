c6786da73c8f28841c7c0e6bc3ffb6f43910a633
noahmiszewski-source <noah.miszewski@skola.taby.se>
2025-11-06 13:31:06 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -5,7 +5,7 @@ print("Hej", namn)
 
 # del 2 - Slumpa ett tal
 
-from random import randint
+#from random import randint
 
 #tal = randint(1, 10)
 #Gissning = int(input("Gissa ett tal mellan 1 till 10: "))
